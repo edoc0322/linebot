cbd906ea9771d28e665acf8f1d69e96b680be893
edoc0322 <bravo0322@gmail.com>
2018-03-07 16:00:03 +0800

    Update demo.js

--- examples/demo.js
+++ examples/demo.js
@@ -125,11 +125,23 @@ bot.on('message', function (event) {
                               return event.reply(str);
 
                         });
-                        //return event.reply(str);
                     }
-                    else if(event.message.text.toString().indexOf('匯率')>-1)
+                    else if(event.message.text.toString().indexOf('匯率')> -1)
                     {
-			    return event.reply('母湯喔');
+						 return event.reply('母湯喔');
+                        /*
+                            request({url:"http://rate.bot.com.tw/xrt?Lang=zh-TW"}
+                            ,function(error,response,body){
+                                if(error)
+                                    return event.reply('Error');
+                                else
+                                {
+                                    var $ = cheerio.load(html)
+                                    var data = $(this).prev();
+                                    return event.reply(data.text);
+                                }
+                            });
+                        */
                     }
                     break;
             }
