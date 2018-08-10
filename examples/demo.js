27d8b0a7b48099ec260471048638f0562d59ced7
edoc0322 <bravo0322@gmail.com>
2018-08-10 17:25:01 +0800

    Update demo.js

--- examples/demo.js
+++ examples/demo.js
@@ -98,7 +98,7 @@ bot.on('message', function (event) {
     imageSize: 'contain',
     columns: [
       {
-        thumbnailImageUrl: 'https://i2.kknews.cc/SIG=mi024v/106o0007r62qp00s60n5.jpg',
+        thumbnailImageUrl: 'https://www.google.com.tw/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjP6KnskOLcAhXO3VMKHXT-B-4QjRx6BAgBEAU&url=https%3A%2F%2Fkknews.cc%2Fcomic%2Fe9a93lz.html&psig=AOvVaw0MeGqdUkWW3eQz-hFid6R_&ust=1533978353024844',
         imageBackgroundColor: '#a8e8fb',
         title: '比價一下 這行比較粗',
         text: '',
