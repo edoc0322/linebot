983dfacb94bf3dc3ef40820b7ec721269c2bd1f2
edoc0322 <bravo0322@gmail.com>
2018-08-10 17:25:38 +0800

    Update demo.js

--- examples/demo.js
+++ examples/demo.js
@@ -101,7 +101,7 @@ bot.on('message', function (event) {
         thumbnailImageUrl: 'https://www.google.com.tw/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjP6KnskOLcAhXO3VMKHXT-B-4QjRx6BAgBEAU&url=https%3A%2F%2Fkknews.cc%2Fcomic%2Fe9a93lz.html&psig=AOvVaw0MeGqdUkWW3eQz-hFid6R_&ust=1533978353024844',
         imageBackgroundColor: '#a8e8fb',
         title: '比價一下 這行比較粗',
-        text: '',
+        text: '01',
         defaultAction: {
           type: 'message',
           label: '點到圖片或標題',
